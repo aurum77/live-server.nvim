b4f9611469dc122971cd55f41730818ffbab3032
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-05 00:59:20 +0300

    Create commands and check if loaded on plugin load

---
+++ plugin/live_server.lua
@@ -0,0 +1,13 @@
+if vim.g.loaded_live_server == 1 then
+	return
+end
+
+vim.g.loaded_live_server = 1
+
+vim.api.nvim_create_user_command("LiveServerStart", function()
+	require("live_server.init").start()
+end, {})
+
+vim.api.nvim_create_user_command("LiveServerStop", function()
+	require("live_server.init").stop()
+end, {})
