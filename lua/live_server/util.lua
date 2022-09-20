4b1c68b5c71acc08b49bba106c21b8e34f9327a7
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-20 18:32:30 +0300

    refactor: rename some vars

--- lua/live_server/util.lua
+++ lua/live_server/util.lua
@@ -1,16 +1,16 @@
 local M = {}
 
-local LIVE_SERVER_PATH = vim.fn.stdpath "data" .. "/live-server/"
+local _LIVE_SERVER_PATH = vim.fn.stdpath "data" .. "/live-server/"
 local loop = vim.loop
 
 M.install = function()
-  if vim.fn.isdirectory(LIVE_SERVER_PATH) == 0 then
-    print("Installing live-server to " .. LIVE_SERVER_PATH)
-    SERVER_HANDLE = loop.spawn("npm", {
-      args = { "i", "live-server", "--prefix", LIVE_SERVER_PATH },
+  if vim.fn.isdirectory(_LIVE_SERVER_PATH) == 0 then
+    print("Installing live-server to " .. _LIVE_SERVER_PATH)
+    INSTALL_HANDLE = loop.spawn("npm", {
+      args = { "i", "live-server", "--prefix", _LIVE_SERVER_PATH },
     }, function()
-      print("live-server has been installed at " .. LIVE_SERVER_PATH)
-      SERVER_HANDLE:close()
+      print("live-server has been installed at " .. _LIVE_SERVER_PATH)
+      INSTALL_HANDLE:close()
     end)
   end
 end
