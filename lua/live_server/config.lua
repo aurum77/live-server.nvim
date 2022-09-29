9767ad3c5470e558bb077c75f79dbc9c969534d0
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-29 23:27:03 +0300

    feat: add no_css_inject option

--- lua/live_server/config.lua
+++ lua/live_server/config.lua
@@ -4,6 +4,7 @@ M.default_config = {
   port = 8080,
   browser_command = "",
   quiet = false,
+  no_css_inject = false,
 }
 
 M.validate = function(user_config)
@@ -23,6 +24,10 @@ M.validate = function(user_config)
     return false
   end
 
+  if type(user_config.no_css_inject) ~= "boolean" then
+    return false
+  end
+
   return true
 end
 
@@ -30,11 +35,13 @@ M.parameterize = function(user_config)
   local port = user_config.port or M.default_config.port
   local browser_command = user_config.browser_command or M.default_config.browser_command
   local quiet = (user_config.quiet or M.default_config.quiet) and "--quiet" or ""
+  local no_css_inject = (user_config.no_css_inject or M.default_config.no_css_inject) and "--no-css-inject" or ""
 
   local params = {
     "--port=" .. port,
     "--browser=" .. browser_command,
     quiet,
+    no_css_inject,
   }
 
   return params
